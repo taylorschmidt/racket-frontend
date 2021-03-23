4bd758f511ef1adb4062a1972d1b2be25edba2bd
taylorschmidt <taylorhillschmidt@gmail.com>
2021-03-23 16:45:51 -0600

    added bar graph

--- components/Chart.js
+++ components/Chart.js
@@ -1,93 +1,100 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 import {
-    Box,
-    Image,
-    Button,
-    HStack,
-    Container,
-    WrapItem,
-    Wrap,
-    Spacer,
-    Flex,
-    Text,
-    Center,
-  } from "@chakra-ui/react";
-import { Doughnut } from 'react-chartjs-2';
+  Box,
+  Image,
+  Button,
+  HStack,
+  Container,
+  WrapItem,
+  Wrap,
+  Spacer,
+  Flex,
+  Text,
+  Center,
+} from "@chakra-ui/react";
+import { Doughnut, HorizontalBar } from "react-chartjs-2";
 
-const SinglesChart = ({win, loss, backhandCount, forehandCount, doublesWins, doublesLosses}) => {
-    
-    const donutChart = {
-        labels: [
-            'Singles Wins',
-            'Singles Losses'
-        ],
-        datasets: [{
-            data:[win, loss],
-            backgroundColor: [
-                '#008566',
-                '#C6ED2C'
-            ],
-            hoverBackgroundColor: [
-                '#00a67f',
-                '#dfff4f'
-            ]
-        }]
-    };
+const SinglesChart = ({
+  doublesData,
+  forehandWin,
+  backhandWin,
+  win,
+  loss,
+  backhandCount,
+  forehandCount,
+  doublesWins,
+  doublesLosses,
+}) => {
+  let backhandPercentage = backhandWin / doublesData.length;
+  let forehandPercentage = forehandWin / doublesData.length;
 
-    const donutChartTwo = {
-        labels: [
-            'Doubles Wins',
-            'Doubles Losses'
-        ],
-        datasets: [{
-            data:[doublesWins, doublesLosses],
-            backgroundColor: [
-                '#008566',
-                '#C6ED2C'
-            ],
-            hoverBackgroundColor: [
-                '#00a67f',
-                '#dfff4f'
-            ]
-        }]
-    };
+  console.log("percentage!", backhandPercentage);
+  console.log("percentage!", forehandPercentage);
 
-    const donutChartThree = {
-        labels: [
-            'Forehand',
-            'Backhand'
-        ],
-        datasets: [{
-            data:[forehandCount, backhandCount],
-            backgroundColor: [
-                '#008566',
-                '#C6ED2C'
-            ],
-            hoverBackgroundColor: [
-                '#00a67f',
-                '#dfff4f'
-            ]
-        }]
-    };
+  const donutChart = {
+    labels: ["Singles Wins", "Singles Losses"],
+    datasets: [
+      {
+        data: [win, loss],
+        backgroundColor: ["#008566", "#C6ED2C"],
+        hoverBackgroundColor: ["#00a67f", "#dfff4f"],
+      },
+    ],
+  };
 
+  const donutChartTwo = {
+    labels: ["Doubles Wins", "Doubles Losses"],
+    datasets: [
+      {
+        data: [doublesWins, doublesLosses],
+        backgroundColor: ["#008566", "#C6ED2C"],
+        hoverBackgroundColor: ["#00a67f", "#dfff4f"],
+      },
+    ],
+  };
 
-    return(
-        <>
-        <Flex flexWrap="wrap">
-            <Box>
-                <Doughnut data={donutChart}/>
-            </Box>
-            <Box>
-                <Doughnut data={donutChartTwo}/>
-            </Box>
-            <Box>
-                <Doughnut data={donutChartThree}/>
-            </Box>
-            </Flex>
-   
-            </>
-    )
-}
+  const donutChartThree = {
+    labels: ["Forehand", "Backhand"],
+    datasets: [
+      {
+        data: [forehandCount, backhandCount],
+        backgroundColor: ["#008566", "#C6ED2C"],
+        hoverBackgroundColor: ["#00a67f", "#dfff4f"],
+      },
+    ],
+  };
 
+  const barChart = {
+    labels: ["Forehand", "Backhand"],
+    datasets: [
+      {
+        label: "Percent Matches Won",
+        data: [forehandPercentage, backhandPercentage],
+        backgroundColor: ["#008566", "#C6ED2C"],
+        hoverBackgroundColor: ["#00a67f", "#dfff4f"],
+        borderWidth: 2,
+      },
+    ],
+  };
 
-export default SinglesChart
+  return (
+    <>
+      <Flex flexWrap="wrap">
+        <Box>
+          <Doughnut data={donutChart} />
+        </Box>
+        <Box>
+          <Doughnut data={donutChartTwo} />
+        </Box>
+        <Box>
+          <Doughnut data={donutChartThree} />
+        </Box>
+        <Box>
+          <HorizontalBar data={barChart} />
+        </Box>
+      </Flex>
+    </>
+  );
+};
+
+export default SinglesChart;
--- components/SinglesStats.js
+++ components/SinglesStats.js
@@ -1,82 +1,104 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 import {
-    Box,
-    Image,
-    Button,
-    HStack,
-    Container,
-    WrapItem,
-    Wrap,
-    Spacer,
-    Flex,
-    Text,
-    Center,
-  } from "@chakra-ui/react";
-  import { Doughnut } from 'react-chartjs-2';
-  import SinglesChart from "./Chart"
+  Box,
+  Image,
+  Button,
+  HStack,
+  Container,
+  WrapItem,
+  Wrap,
+  Spacer,
+  Flex,
+  Text,
+  Center,
+} from "@chakra-ui/react";
+import { Doughnut } from "react-chartjs-2";
+import SinglesChart from "./Chart";
 
+const SinglesStats = ({ data, doubles }) => {
+  const [loading, setLoading] = useState(true);
 
-const SinglesStats = ({data, doubles}) => {
-    const [loading, setLoading] = useState(true)
+  let winCount = 0;
+  let lossCount = 0;
+  let doublesWins = 0;
+  let doublesLosses = 0;
+  let forehandCount = 0;
+  let backhandCount = 0;
+  let forehandWin = 0
+  let backhandWin = 0
 
-    let winCount = 0;
-    let lossCount = 0
-    let doublesWins = 0
-    let doublesLosses = 0
-    let forehandCount = 0
-    let backhandCount = 0
+  const countingWins = () => {
+    return data.map((data, index) => {
+      if (data.win) {
+        winCount += 1;
+      } else if (!data.win) {
+        lossCount += 1;
+      }
+    });
+  };
 
+  const countingDoublesWins = () => {
+    return doubles.map((data, index) => {
+      if (data.win) {
+        doublesWins += 1;
+      } else if (!data.win) {
+        doublesLosses += 1;
+      }
+    });
+  };
 
-    const countingWins = () => {
-        return data.map((data, index) => {
-            if (data.win) {
-              winCount += 1;
-            } else if (!data.win) {
-                lossCount += 1
-            }
-    })
-    }
+  const countingHands = () => {
+    return doubles.map((data, index) => {
+      if (data.hand === "forehand") {
+        forehandCount += 1;
+      } else if (data.hand === "backhand") {
+        backhandCount += 1;
+      }
+    });
+  };
 
-    const countingDoublesWins = () => {
-        return doubles.map((data, index) => {
-            if (data.win) {
-              doublesWins += 1;
-            } else if (!data.win) {
-                doublesLosses += 1
-            }
-    })
-    }
-
-    const countingHands = () => {
-        return doubles.map((data, index) => {
-        if(data.hand === "forehand") {
-            forehandCount += 1
-           
-        } else if (data.hand === "backhand") {
-            backhandCount += 1
-            
+  const countingHandWins = () => {
+    return doubles.map((data, index) => {
+        if (data.hand === "forehand" && data.win ) {
+          forehandWin += 1;
+        } else if (data.hand === "backhand" && data.win) {
+          backhandWin += 1;
         }
-        })
-        
-    }
+      });
+  }
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setLoading(false)
-        }, 1000)
-    })
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  });
 
-    return(
+  return (
+    <>
+      {countingWins()}
+      {countingDoublesWins()}
+      {countingHands()}
+      {countingHandWins()}
+      {!loading && (
         <>
-            {countingWins()}
-            {countingDoublesWins()}
-            {countingHands()}
-           {!loading && (<Box>
-
-<SinglesChart win={winCount} loss={lossCount} doublesWins={doublesWins} doublesLosses={doublesLosses} forehandCount={forehandCount} backhandCount={backhandCount}/>
-</Box>)}
+        <Box>
+          <SinglesChart
+            doublesData={doubles}
+            win={winCount}
+            loss={lossCount}
+            doublesWins={doublesWins}
+            doublesLosses={doublesLosses}
+            forehandCount={forehandCount}
+            backhandCount={backhandCount}
+            forehandWin={forehandWin}
+            backhandWin={backhandWin}
+            
+          />
+        </Box>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default SinglesStats
+export default SinglesStats;
